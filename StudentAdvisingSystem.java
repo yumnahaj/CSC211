80769676a989f011ad5826dd5747fbf22399040a
mm44505 <mariam1525jjj@gmail.com>
2026-05-14 23:31:51 +0300

    Update StudentAdvisingSystem.java

--- StudentAdvisingSystem.java
+++ StudentAdvisingSystem.java
@@ -6,16 +6,21 @@ public class StudentAdvisingSystem
     Scanner input = new Scanner(System.in);
     AdvisingSystem advisingSystem = new AdvisingSystem();
     int choice ;
+    System.out.println("======================================\nWelcome to the Student Advising System!\n======================================");
+
     do
     {
-      System.out.println("======================================\nWelcome to the Student Advising System!\n======================================\nPlease choose an option:");
-      System.out.println("1.Add a student\n2.Search for a student\n3.Delete a student\n4.Schedule a meeting/workshop\n5.Print meeting/workshop details\n6.Print students by name\n7.Print all meetings/workshops alphabetically\n8.Exit");
+      System.out.println("======================================\nPlease choose an option:\n1.Add a student\n2.Search for a student\n3.Delete a student\n4.Schedule a meeting/workshop\n5.Print meeting/workshop details\n6.Print All Students\n7.Print all meetings/workshops alphabetically\n8.Load Students from CSV\n9.Load Events from CSV\n10.Exit");
       System.out.println("Enter your choice: ");
       choice = input.nextInt();
-      while(choice !=1 && choice !=2 && choice !=3 && choice !=4 && choice !=5 && choice !=6 && choice !=7 && choice !=8)
+      input.nextLine(); // consume the newline character
+
+      while(choice !=1 && choice !=2 && choice !=3 && choice !=4 && choice !=5 && choice !=6 && choice !=7 && choice !=8&& choice !=9 && choice !=10)
       {
          System.out.println("Please enter a valid choice:");
          choice = input.nextInt();
+         input.nextLine(); // consume the newline character
+
       }
       switch(choice)
       {
@@ -24,17 +29,16 @@ public class StudentAdvisingSystem
               String name = input.nextLine();
               System.out.println("Enter the student's ID:");
               int id = input.nextInt();
-              input.nextLine();
+              input.nextLine(); // consume the newline character
               System.out.println("Enter the student's email address:");
               String email = input.nextLine();
               System.out.println("Enter the student's major:");
               String major = input.nextLine();
               System.out.println("Enter the student's year level");
               int yearLevel = input.nextInt();
-              input.nextLine();
+              input.nextLine(); // consume the newline character
               System.out.println("Enter any notes for the student:");
               String notes = input.nextLine();
-
               Student student = new Student(name, id, email,major,yearLevel, notes);
               boolean addedSuccessfully =  advisingSystem.addStudent(student);
               if(addedSuccessfully)
@@ -47,16 +51,16 @@ public class StudentAdvisingSystem
               System.out.println("1.Email\n2.StudentID");
               System.out.println("Enter your choice: ");
               int searchChoice = input.nextInt();
+              input.nextLine(); // consume the newline character
               while(searchChoice !=1 && searchChoice !=2 )
               {
                  System.out.println("Please enter a valid choice:");
                  searchChoice = input.nextInt();
+                 input.nextLine(); // consume the newline character
               }
-              input.nextLine();
-
               switch(searchChoice)
               {
-                case 1:
+                case 1://search by email
                     System.out.println("Enter the student's email:");
                     String findByEmail = input.nextLine();
                     if(advisingSystem.searchStudentByEmail(findByEmail)!= null)
@@ -64,7 +68,7 @@ public class StudentAdvisingSystem
                     else
                       System.out.println("Student not found:(");
                     break;
-                case 2:
+                case 2://search by ID
                     System.out.println("Enter the student's ID:");
                     int findByID = input.nextInt();
                     if(advisingSystem.searchStudentById(findByID)!= null)
@@ -92,22 +96,24 @@ public class StudentAdvisingSystem
               System.out.println("1.Workshop\n2.Meeting");
               System.out.println("Enter your choice: ");
               int typeChoice = input.nextInt();
+              input.nextLine(); // consume the newline character
               while(typeChoice !=1 && typeChoice !=2 )
               {
                  System.out.println("Please enter a valid choice:");
                  typeChoice = input.nextInt();
+                 input.nextLine(); // consume the newline character
               }
-              input.nextLine();
-
               switch(typeChoice)
               {
-                  case 1:
+                  case 1://schedule a workshop
                     System.out.println("Enter workshop title:");
                     String workshopTitle = input.nextLine();
                     System.out.println("Enter student IDs separated by a comma:");
                     String studentIds = input.nextLine();
-                    StudentList studentList = new StudentList();
-                   /**/  int [] studentIdArray = Arrays.stream(studentIds.split(",")).mapToInt(Integer::parseInt).toArray();
+                    String[] arrayID = studentIds.split(",");
+                    int[] studentIdArray = new int[arrayID.length];
+                    for (int i = 0; i < arrayID.length; i++) 
+                        studentIdArray[i] = Integer.parseInt(arrayID[i]);
                     System.out.println("Enter workshop start date and time (MM/DD/YYYY HH:MM):");
                     String startWorkshopDateTime = input.nextLine();
                     DateTime startDateTime = new DateTime(startWorkshopDateTime);
@@ -123,12 +129,12 @@ public class StudentAdvisingSystem
                       System.out.println("Failed to schedule the workshop");
               
                     break;
-                  case 2:
+                  case 2://schedule a meeting
                     System.out.println("Enter meeting title:");
                     String meetingTitle = input.nextLine();
                     System.out.println("Enter student ID:");
                     int studentID= input.nextInt();
-                    input.nextLine();
+                    input.nextLine(); // consume the newline character
                     System.out.println("Enter meeting start date and time (MM/DD/YYYY HH:MM):");
                     String startMeetingDateTime = input.nextLine();
                     DateTime startDateTimeM = new DateTime(startMeetingDateTime);
@@ -147,39 +153,55 @@ public class StudentAdvisingSystem
               }
             break;
         case 5://Print meeting/workshop details
-            System.out.println("Enter search criteria");
+              System.out.println("Enter search criteria");
               System.out.println("1.Title\n2.Student name");
               System.out.println("Enter your choice: ");
               int printChoice = input.nextInt();
+              input.nextLine(); // consume the newline character
               while(printChoice !=1 && printChoice !=2 )
               {
                  System.out.println("Please enter a valid choice:");
                  printChoice = input.nextInt();
+                 input.nextLine(); // consume the newline character
               }
 
               switch(printChoice)
               {
-                 case 1:
+                 case 1://print by title
                     System.out.println("Enter the event title:");
                     String printByTitle = input.nextLine();
                     advisingSystem.printEventDetailsByTitle(printByTitle);
                     break;
-                case 2:
+                case 2://print by student name
                     System.out.println("Enter the student name:");
                     String printByStudentName = input.nextLine();
                     advisingSystem.printEventDetailsByStudentName(printByStudentName);
                     break;
               }
             break;
-        case 6://Print students by name
-              System.out.println("Enter the student name:");
-              String printStudentList = input.nextLine();
-              advisingSystem.printStudentsByName(printStudentList);
+        case 6://Print All Students
+              advisingSystem.printAllStudents();
             break;
         case 7://Print all meetings/workshops alphabetically
               advisingSystem.printAllEventsAlphabetically();
             break;
-        case 8://Exit
+        case 8://Load Students from CSV
+              System.out.println("Enter the file path:");
+              String studentFilePath = input.nextLine();
+              if(advisingSystem.loadStudentsFromCSV(studentFilePath))
+                 System.out.println("Students loaded successfully^-^");
+              else 
+                 System.out.println("Failed to load students:(Please check the file path and try again)");
+              break;
+        case 9://Load Events from CSV
+              System.out.println("Enter the file path:");
+              String eventFilePath = input.nextLine();
+              if(advisingSystem.loadEventsFromCSV(eventFilePath))
+                 System.out.println("Events loaded successfully^-^");
+              else 
+                 System.out.println("Failed to load events:(Please check the file path and try again)");
+              break;    
+        case 10://Exit
             System.out.println("Goodbye!\nHave a nice day^-^");
             System.exit(0);
              break;
@@ -190,7 +212,7 @@ public class StudentAdvisingSystem
 
         
     }
-    while(choice!=8);
+    while(choice!=10);
     
   }
     
